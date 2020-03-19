df96f6ec52c378c121409a405167669538ac70a1
mbzomowski <mbzomowski@gmail.com>
2020-03-19 02:07:54 -0700

    Now calculates the variance in respect to the average returns, as it should have to begin with. Also calculates covariance between all provided securities

--- returnvariance.py
+++ returnvariance.py
@@ -1,5 +1,8 @@
 import yfinance as yf
 
+STARTING_YEAR = 2015
+END_YEAR = 2020
+
 # This list can be adjusted to include whatever securities are needed
 tickers = ['MSFT', 'KO', 'HAL']
 
@@ -9,34 +12,46 @@ data = yf.download(tickers, start='2015-01-01', end='2020-01-10', period='1d', t
 # We will only use the adjusted close prices, as I believe they account for splits
 daily_price = data['Adj Close']
 
+mean_security_returns = []
+yearly_security_returns = []
+
 for sym in tickers:
     yearly_returns = []
-    yearly_variance = []
-    for year in range(2015, 2020):
+    for year in range(STARTING_YEAR, END_YEAR):
         # Calculate the pct change for each year
         current_year_start = daily_price[sym][str(year)].iloc[0]
         next_year_start = daily_price[sym][str(year+1)].iloc[0]
         security_return = (next_year_start - current_year_start) / current_year_start
         yearly_returns.append(security_return)
 
-        # Calculate the variance for each year
-        avg_yearly_price = daily_price[sym][str(year)].mean()
-        year_mean_sq_avg = daily_price[sym][str(year)].apply(lambda x: (x - avg_yearly_price)**2)
-        num_trading_days = daily_price[sym][str(year)].size
-        year_variance = year_mean_sq_avg.sum() / num_trading_days
-
-        # I normalize the yearly variance by the yearly mean here
-        year_variance /= avg_yearly_price
-        yearly_variance.append(year_variance)
+    yearly_security_returns.append(yearly_returns)
 
     # Weight the returns and variances for more recent years heavier than earlier years
-    weighted_expected_return = sum([yearly_returns[i-1] * 0.1 * i for i in range(1, 5)])
-    weighted_variance = sum([yearly_variance[i-1] * 0.1 * i for i in range(1, 5)])
+    mean_yearly_return = sum(yearly_returns) / (END_YEAR - STARTING_YEAR)
+
+    mean_security_returns.append(mean_yearly_return)
+
+    # Calculate the variance of the returns
+    return_variance = 0
+    for ret in yearly_returns:
+        return_variance += (ret - mean_yearly_return)**2
+    return_variance /= (END_YEAR - STARTING_YEAR - 1)
 
     # Print data for each stock
     print(sym)
     print("Expected return:")
-    print(weighted_expected_return)
-    print("Expected normalized variance:")
-    print(weighted_variance)
+    print(mean_yearly_return)
+    print("Expected variance of returns:")
+    print(return_variance)
     print()
+
+# Calculate the covariance between all combinations of securities
+for i in range(len(tickers)):
+    for j in range(i + 1, len(tickers)):
+        cov = 0
+        print("Covariance between {} and {}".format(tickers[i], tickers[j]))
+        for k in range(END_YEAR - STARTING_YEAR):
+            cov += (yearly_security_returns[i][k] - mean_security_returns[i])\
+                * (yearly_security_returns[j][k] - mean_security_returns[j])
+        cov /= (END_YEAR - STARTING_YEAR - 1)
+        print(cov)
